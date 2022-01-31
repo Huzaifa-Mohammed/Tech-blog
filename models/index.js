a48410639f821ea47419be3d1bddd7640b60b626
Huzaifa Mohammed <huzirizwan@gmail.com>
2022-01-31 00:48:21 -0500

    models and routes ready

---
+++ models/index.js
@@ -0,0 +1,37 @@
+const User = require('./User');
+const Post = require("./Post");
+const Comment = require('./Comment');
+
+// USER RELATIONSHIPS
+
+User.hasMany(Post, {
+    foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "SET NULL",
+});
+
+
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "SET NULL",
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "SET NULL",
+});
+
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+    onDelete: "SET NULL",
+});
+
+module.exports = { Post, User, Comment };
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const routes = require('./routes');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//turn on Routes
+app.use(routes);
+
+// turn on connection to db and server
+// we use the sequelize.sync() method to establish the connection to the database
+// The "sync" part means that this is Sequelize taking the models and connecting them to associated database tables.
+// If it doesn't find a table, it'll create it for you!
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now Listening'));
+});
